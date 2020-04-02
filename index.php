f54f7b5de0cd49891c562ef2e92c2877f8cedadc
Michel <97466@student.drenthecollege.nl>
2020-04-02 12:10:08 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+   <<head>
+   </head>
+   <body>
+     <h1>Startpagina</h1>
+     <p>Hieronder komen een aantal links te staan</p>
+     <ul><a href="./contact.php">linkje</a></ul>
+     <ul><a href="./contact.php">linkje1</a></ul>
+     <ul><a href="./contact.php">linkje2</a></ul>
+   </body>
+</html>
