431ef4c7a433ad264133732d29bf2ce9a8cf8d8a
Michel <97466@student.drenthecollege.nl>
2020-04-02 12:40:54 +0200

    bug gemaakt

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <html>
-  <head>
-   </head>
    <body>
      <h1>Startpagina</h1>
      <p>Hieronder komen een aantal links te staan</p>
      <ul><a href="./contact.php">linkje</a></ul>
      <ul><a href="./contact.php">linkje1</a></ul>
-     <ul><a href="./contact.php">linkje2</a></ul>
+     <ul><a href="./contact241.php">linkje2</a></ul>
    </body>
 </html>
